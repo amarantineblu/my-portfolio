dc722a1fec2ace6c256fd97c15f94792d0c6aaba
amarantineblu <amaranthblu@yahoo.com>
2026-05-29 12:05:39 +0100

    color change to big text

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
 const navBarToggleBtn = document.querySelector(".navbar-toggler");
 const menu_bar = document.querySelector("#menu-bar");
 
+
+const currentPage = window.location.pathname;
+const navLinks = document.querySelectorAll(" nav ul li a");
+
+let currentNavLink;
+currentPage === currentNavLink && activeNavLink;
+console.log(navLinks);
+navLinks.forEach(navLink => {
+  navLink.addEventListener('click', (e) => {
+    console.log('the current page is' + e.target.pathname);
+    console.log('the current page is' + currentPage);
+    e.target.getAttribute('href')  === currentPage.split('/').pop() && e.target.classList.add('active');
+  });
+});
+
+
+
 let count = 0;
 navBarToggleBtn.addEventListener("click", (e) => {
   const menubarClassName = menu_bar.classList;
